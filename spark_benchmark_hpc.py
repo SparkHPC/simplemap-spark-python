62f6cc2ae091286958fd89ec4cd2d468946cb00b
George K. Thiruvathukal <gkt@cs.luc.edu>
2018-04-25 21:37:35 +0000

    fixes after testing on Cooley

--- spark_benchmark_hpc.py
+++ spark_benchmark_hpc.py
@@ -5,7 +5,7 @@ Apache Spark performance.
 
 """
 
-from __future__ import print_function
+#from __future__ import print_function
 
 import argparse
 import glob
@@ -13,9 +13,10 @@ import json
 import numpy as np
 import os
 import re
+import sys
 import time
 
-from pyspark import SparkContext
+from pyspark import SparkContext, SparkConf
 
 # Used by RDD lambda to generate the work
 
@@ -61,10 +62,8 @@ def parse_args():
 
     # Options for generating RDD (in memory)
     parser.add_argument("-g", "--generate", action="store_true", default=False, help="generate data (boolean)")
-    parser.add_argument("-b", "--blocks", type=int,
-                        default=0, help="number of blocks")
-    parser.add_argument("-s", "--block_size", type=int,
-                        default=0, help="block size")
+    parser.add_argument("-b", "--blocks", type=int, default=0, help="number of blocks")
+    parser.add_argument("-s", "--block_size", type=int, default=0, help="block size")
 
     # These are all used to define amount of paralleism (for sc.parallelize).
 
@@ -78,48 +77,53 @@ def parse_args():
     # Reporting
     parser.add_argument('-j', "--json", type=str,
                         default=None, help="where to write the report")
-    parser.add_argument("-z", "--size", type=int, required=True,
-                        help="input size (in mb, for reporting)")
+    #parser.add_argument("-z", "--size", type=int, required=True,
+    #                    help="input size (in mb, for reporting)")
     return parser.parse_args()
 
 
 def main():
 
-    sc = SparkContext(appName="simplemap-spark-python")
+
+    sconf = SparkConf()
+    sc = SparkContext(appName="simplemap-spark-python", conf=sconf)
+    python_version = sys.version
+    spark_python_version = sc.pythonVer
+    print("Python %s, Spark %s" % (python_version, spark_python_version))
     args = parse_args()
     timers = SimpleTimer()
 
     # read input files or generate input data
     timers.init_and_start("rdd")
     if args.generate:
-        gen_num_blocks = args.blocks,
+        gen_num_blocks = args.blocks
         gen_block_size = args.block_size
-        rdd = sc.parallelize(range(gen_num_blocks),
-                             args.nodes * args.cores * args.nparts)
+        print("gen_num_blocks",gen_num_blocks)
+        x = range(0, gen_num_blocks)
+        rdd = sc.parallelize(range(0, gen_num_blocks), args.nodes * args.cores * args.nparts)
         gen_block_count = gen_block_size*1E6/24
         A = rdd.map(lambda n: generate(n, gen_block_count))
     else:
         print("either --generate must be specified")
         sc.stop()
-        from sys import exit
-        exit(-1)
+        sys.exit(-1)
 
     timers.stop("rdd")
 
     timers.init_and_start("cache")
     A.cache()
-    timers.end("cache")
+    timers.stop("cache")
 
     # apply simple operation (V'=V+V0)
 
     timers.init_and_start("shift")
     shift = np.array([25.25, -12.125, 6.333], dtype=np.float64)
     B = A.map(lambda x: do_shift(x, shift))
-    timers.end("shift")
+    timers.stop("shift")
 
     timers.init_and_start("average")
     B_avg = B.map(do_average)
-    timers.end("average")
+    timers.stop("average")
 
     if args.json:
       with open(args.json, "w") as report:
@@ -144,19 +148,19 @@ class SimpleTimer(object):
         self.init(name)
         self.start(name)
 
-    def start(self, name):
+    def stop(self, name):
         self.timings[name]['stop'] = time.time()
 
     def copy(self, name, new_name):
         self.timings[new_name] = self.get(name, {'start': 0, 'stop': 0})
 
-    def get(self, name):
-        timer_entry = self.timings.get({})
-        delta_t = timer_entry.get('end', 0) - timer_entry.get('start', 0)
+    def get_name(self, name):
+        timer_entry = self.timings.get(name, {})
+        delta_t = timer_entry.get('stop', 0) - timer_entry.get('start', 0)
         return max(delta_t, 0)
 
-    def get_all():
-      return {k : self.get(k) for k in self.timings.keys()}
+    def get_all(self):
+      return {k : self.get_name(k) for k in self.timings.keys()}
 
 
 if __name__ == "__main__":
